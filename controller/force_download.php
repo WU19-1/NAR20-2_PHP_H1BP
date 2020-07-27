0fbbd2b96cd4b5d5396d420dcb32119f3508824e
WU19-1 <53069607+WU19-1@users.noreply.github.com>
2020-07-27 09:51:13 +0700

    Update force_download.php

--- controller/force_download.php
+++ controller/force_download.php
@@ -4,7 +4,8 @@
     // echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . "/controller/download.php?path=" . $file;
     echo "<script>";
     echo "window.open(\"".(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST']."/controller/downloader.php?path=".$file."\");";
-    echo "history.back();";
+//     echo "history.back();";
 //     echo "location.reload();";
     echo "</script>";
+    header('Location : ' . $_SERVER['HTTP_REFERER']);
 ?>
