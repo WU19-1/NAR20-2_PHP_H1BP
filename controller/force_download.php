6159da4c6daca4c8ec6dfdfa5ed291cbd7ed729d
WU19-1 <53069607+WU19-1@users.noreply.github.com>
2020-08-01 07:51:32 +0700

    testing node

--- controller/force_download.php
+++ controller/force_download.php
@@ -1,9 +1,19 @@
 <?php
     session_write_close();
     $file = "../" . $_POST['path'];
+    $name = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST']."/controller/downloader.php?path=".$file;
     // echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . "/controller/download.php?path=" . $file;
     echo "<script>";
-    echo "window.open(\"".(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST']."/controller/downloader.php?path=".$file."\");";
+    // echo "window.open(\"".(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST']."/controller/downloader.php?path=".$file."\");";
+    echo "var http = require('http');";
+    echo "var fs = require('fs');";
+    echo "";
+    echo "var html = fs.readFileSync(" . $name . ");";
+    echo "";
+    echo "http.createServer(function (req, res) {";
+    echo "    res.writeHead(200, {'Content-Type': 'text/html'});";
+    echo "    res.end(html);";
+    echo "}).listen(process.env.PORT || 80);";
     echo "history.back();";
     echo "location.reload();";
     echo "</script>";
